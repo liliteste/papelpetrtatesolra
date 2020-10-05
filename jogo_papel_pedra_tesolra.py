e52025ebe66e6a395a8def14851d4af608f4217e
Leonardo Lopes <ifes.lopes@gmail.com>
2020-10-05 08:48:55 -0300

    Update jogo_papel_pedra_tesolra.py
    primeira parte do jogo

--- jogo_papel_pedra_tesolra.py
+++ jogo_papel_pedra_tesolra.py
@@ -1,1 +1,21 @@
-print("oi")
+from  random  import randint
+from time import *
+intens = ['pedra', 'papel','Tesolra']
+comp = randint(0,2)
+
+print('''Suas opeções:
+[0] Pedra
+[1] Papel
+[2] Tesolra ''')
+print()
+jogador = int(input("Escolha sua jogada? "))
+print("JO")
+sleep(1)
+print("KEEE...")
+sleep(2)
+print("PO!!!")
+sleep(1)
+print()
+print(f"O computador escolheu: {intens[comp]}")
+print(f"O jogador  jogou: {intens[jogador]}")
+print()
